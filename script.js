22eea8d1f01ed8b9fe66002939e15a1ef7f2def4
rephthal <rephthal@gmail.com>
2026-08-20 23:22:25 +0900

    all

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function toggleMenu() {
     const menu = document.getElementById("menu");
     menu.classList.toggle("active");
 }
+function showPage(pageId) {
+    document.querySelectorAll(".page").forEach(page => {
+        page.classList.remove("active");
+    });
+
+    document.getElementById(pageId).classList.add("active");
+
+    document.getElementById("menu").classList.remove("active");
+}
